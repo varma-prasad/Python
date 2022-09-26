436933092ad75d3f4754252dc3de6bb9c483b7de
varma-prasad <108605375+varma-prasad@users.noreply.github.com>
2022-09-26 17:10:00 +0530

    Conditional statement

---
+++ e_vidya/IfElse.py
@@ -0,0 +1,8 @@
+a=input('enter a Number')
+b=input('Enter another number')
+if a>b:
+    print('{} is greater than {}'.format(a,b))
+    print('hello')
+else:
+    print('{} is greater than {}'.format(b,a))
+    print('Bye')
