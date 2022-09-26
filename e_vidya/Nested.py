209f45f2a518d1338d7bd05b116c9e971e61f081
varma-prasad <108605375+varma-prasad@users.noreply.github.com>
2022-09-26 17:10:42 +0530

    Nested if elif else

---
+++ e_vidya/Nested.py
@@ -0,0 +1,8 @@
+a=input('enter a Number')
+b=input('Enter second number')
+if a<b:
+    print('{} is less than {}'.format(a,b))
+elif a>b:
+    print('{} is greater than {}'.format(a,b))
+else:
+    print('{} is equal to {}'.format(a,b))
