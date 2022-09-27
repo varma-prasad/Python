16ab6f155eb707e9c1aa444a1766b8c5d35677cd
varma-prasad <108605375+varma-prasad@users.noreply.github.com>
2022-09-27 20:51:59 +0530

    user Defined Range Function

---
+++ e_vidya/my_range.py
@@ -0,0 +1,28 @@
+def main():
+    for i in my_range(2,4,5,6):
+        print(i,end=" ")
+
+def my_range(*args):
+    n=len(args)
+    start=0
+    step=1
+    if n<1:
+        print('error')
+        return
+    elif n==1:
+        stop=args[0]
+    elif n==2:
+        (start,stop)=args
+    elif n==3:
+        (start,stop,step)=args
+    else:
+        print('error')
+        return
+    i=start
+    while i<=stop:
+        yield i
+        i=i+step
+        
+    
+
+main()
