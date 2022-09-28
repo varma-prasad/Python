2a6b909d1603d055557d82dceea0fdc4840c09ce
varma-prasad <108605375+varma-prasad@users.noreply.github.com>
2022-09-28 12:46:07 +0530

    String methods

---
+++ e_vidya/string1.py
@@ -0,0 +1,10 @@
+name="varma prasad"
+print(name.count('a'))
+print(name.replace('r','R'))
+print(name.find(' '))
+print(name.startswith('va'))
+print(name.endswith('da'))
+print(name.isdigit())
+print(name.upper())
+print(name.split('a'))
+
