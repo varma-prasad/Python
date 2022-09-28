540ce861914bd5a441dfdbc51fb181f676282afb
varma-prasad <108605375+varma-prasad@users.noreply.github.com>
2022-09-28 13:55:55 +0530

    Numpy Introduction

---
+++ e_vidya/num_py.py
@@ -0,0 +1,11 @@
+import numpy as np
+student_marks=np.array([1,2,3,4,5,6])
+print(student_marks)
+print(student_marks.shape)
+l=[1,2,3,4,5,6]
+print(l)
+car=[['maruti','toyota','tata'],[1000,2000,3000],[10,20,30]]
+car_array=np.array(car)
+print(car_array)
+print(car_array.shape)
+print(car_array.dtype)
