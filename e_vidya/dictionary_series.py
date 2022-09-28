78fe5a8366df49020657841d17bae73f2ee7bd58
varma-prasad <108605375+varma-prasad@users.noreply.github.com>
2022-09-28 17:11:01 +0530

    Series by Dictionary

---
+++ e_vidya/dictionary_series.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+car_price_dict={'swift':70000,
+                'civic':800000,
+                'Altis':180000,
+                'Gallardo':300000}
+series=pd.Series(car_price_dict)
+print(series)
